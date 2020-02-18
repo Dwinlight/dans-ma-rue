15af6a0e3be7378c81531f5800145028b030df8a
Dwinlight <corentinpouget15@gmail.com>
2020-02-18 16:33:04 +0100

    Compter le nombre d'anomalies par arrondissement

--- services/stats.js
+++ services/stats.js
@@ -2,8 +2,34 @@ const config = require('config');
 const indexName = config.get('elasticsearch.index_name');
 
 exports.statsByArrondissement = (client, callback) => {
-    // TODO Compter le nombre d'anomalies par arondissement
-    callback([]);
+    client.search({
+        index : "dansmarue",
+        size: 0,
+        body:{
+            aggs : {
+                "arrondissement" : {
+                    "terms": {
+                        "field": "arrondissement.keyword"
+                    }
+                }
+        }
+    }
+    }).then((resp) =>{
+        const tab =[];
+        const aggr = resp.body.aggregations.arrondissement.buckets;
+        
+        aggr.forEach((element) => {
+            tab.push({
+                "arrondissement": element["key"],
+                "count": element["doc_count"]
+            });
+
+
+        
+    });
+        callback(tab);
+    }, err => console.error(err.meta.body.error));
+
 }
 
 exports.statsByType = (client, callback) => {
